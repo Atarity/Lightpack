daeb40bdfcdbf4417334c492c6a6c45287915b76
brunql <brunql@gmail.com>
2010-11-12 19:56:12 +0300

    Copyleft firmware sources, main.cpp added avrdude command line (u can find here FUSES)

--- Firmware/inc/usbconfig.h
+++ Firmware/inc/usbconfig.h
@@ -8,6 +8,31 @@
  * This Revision: $Id: usbconfig-prototype.h 767 2009-08-22 11:39:22Z cs $
  */
 
+/* 
+ * usbconfig.h
+ *
+ *        Edit: Mike Shatohin (brunql)
+ *     Project: AmbilightUSB
+ *
+ *  AmbilightUSB is very simple implementation of the backlight for a laptop
+ *
+ *  Copyright (c) 2010 Mike Shatohin, mikeshatohin [at] gmail.com
+ *
+ *  AmbilightUSB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AmbilightUSB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef __usbconfig_h_included__
 #define __usbconfig_h_included__
 
--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -1,14 +1,40 @@
 /*
  * main.c
  *
- *  Created on: 21.07.2010
- *      Author: brunql
+ *   Created on: 21.07.2010
+ *       Author: Mike Shatohin (brunql)
  *
  *      Project: AmbilightUSB
  *      	MCU: ATtiny44
  *        Clock: 12MHz
+ *
+ * Command to fill flash and set fuses:       
+ *
+ * avrdude	-pt44 -cusbasp -u
+ *          -Uflash:w:AmbilightUSB.hex:a
+ *          -Ulfuse:w:0xee:m
+ *          -Uhfuse:w:0xdf:m
+ *          -Uefuse:w:0xff:m
+ *
+ *  AmbilightUSB is very simple implementation of the backlight for a laptop
+ *
+ *  Copyright (c) 2010 Mike Shatohin, mikeshatohin [at] gmail.com
+ *
+ *  AmbilightUSB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AmbilightUSB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
+ 
 #include <avr/io.h>
 #include <avr/interrupt.h> /* for sei() */
 #include <util/delay.h>
