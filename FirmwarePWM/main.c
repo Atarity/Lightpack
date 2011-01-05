70237625715210e56cee239b0a63ae031270a945
brunql <brunql@gmail.com>
2011-01-05 14:41:37 +0300

    FirmwarePWM: send OK or FAIL then receive data; all action in interrupts.
    Signed-off-by: brunql <brunql@gmail.com>

--- FirmwarePWM/main.c
+++ FirmwarePWM/main.c
@@ -5,7 +5,7 @@
  *       Author: Mike Shatohin (brunql)
  *
  *      Project: AmbilightUSB
- *      	MCU: ATmega8
+ *          MCU: ATmega8
  *        Clock: 12MHz
  *
  *  This firmware for the microcontroller, which controls the LEDs (PWM).
@@ -42,7 +42,7 @@
 #include "main.h"
 #include "74HC595.h"
 #include "RGB.h"
-
+#include "commands.h"
 
 //
 // Global variables
@@ -53,14 +53,14 @@ volatile uint8_t update_colors = FALSE;
 
 // Contains colors using now in PWM generation
 volatile uint8_t colors[LEDS_COUNT][3] = {
-		{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-		{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
 };
 
 // Contains new colors (comes from PC) for update with smooth (if it is used)
 volatile uint8_t colors_new[LEDS_COUNT][3] = {
-		{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-		{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
 };
 
 // Current PWM generation level
@@ -74,8 +74,8 @@ volatile uint8_t pwm_level_max = 64;
 volatile uint8_t smooth = 0x00;
 volatile uint8_t smooth_delay = 0x00;
 volatile uint8_t smooth_step[LEDS_COUNT][3] = {
-		{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-		{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
 };
 
 volatile int16_t max_diff = 0x00;
@@ -85,155 +85,161 @@ volatile uint8_t receive_color_index = 0x00;
 
 
 
-
 static inline void UpdateSmoothStep(void)
 {
-	// Check how fast is this...
+    // Check how fast is this...
 
-	// First find MAX diff between old and new colors, and save all diffs in each smooth_step
-	for(uint8_t color=0; color < 3; color++){
-		for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
-			int16_t diff = colors[led_index][color] - colors_new[led_index][color];
-			if(diff < 0) diff *= -1;
+    // First find MAX diff between old and new colors, and save all diffs in each smooth_step
+    for(uint8_t color=0; color < 3; color++){
+        for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
+            int16_t diff = colors[led_index][color] - colors_new[led_index][color];
+            if(diff < 0) diff *= -1;
 
-			if(diff > max_diff) max_diff = diff;
+            if(diff > max_diff) max_diff = diff;
 
-			smooth_step[led_index][color] = (uint8_t)diff;
-		}
-	}
+            smooth_step[led_index][color] = (uint8_t)diff;
+        }
+    }
 
-	// To find smooth_step which will be using max_diff divide on each smooth_step
-	for(uint8_t color=0; color < 3; color++){
-		for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
-			smooth_step[led_index][color] = (uint8_t) max_diff / smooth_step[led_index][color];
-		}
-	}
+    // To find smooth_step which will be using max_diff divide on each smooth_step
+    for(uint8_t color=0; color < 3; color++){
+        for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
+            smooth_step[led_index][color] = (uint8_t) max_diff / smooth_step[led_index][color];
+        }
+    }
+
+    smooth = 0x00;
 }
 
 
-void SmoothlyUpdateColors(void)
+static inline void SmoothlyUpdateColors(void)
 {
-	// Array smooth_step evaluated when new color comes from PC
+    // Array smooth_step evaluated when new color comes from PC
 
-	for(uint8_t color=0; color < 3; color++){
-		for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
-			if(smooth % smooth_step[led_index][color] == 0){
-				if(colors[led_index][color] < colors_new[led_index][color]){
-					colors[led_index][color] += 1;
-				}
-				if(colors[led_index][color] > colors_new[led_index][color]){
-					colors[led_index][color] -= 1;
-				}
-				update_colors = TRUE;
-			}
-		}
-	}
+    for(uint8_t color=0; color < 3; color++){
+        for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
+            if(smooth % smooth_step[led_index][color] == 0){
+                if(colors[led_index][color] < colors_new[led_index][color]){
+                    colors[led_index][color] += 1;
+                }
+                if(colors[led_index][color] > colors_new[led_index][color]){
+                    colors[led_index][color] -= 1;
+                }
+                update_colors = TRUE;
+            }
+        }
+    }
 
-	if(++smooth >= max_diff){
-		smooth = 0x00;
-		update_colors = FALSE;
-	}
+    if(++smooth >= max_diff){
+        smooth = 0x00;
+        update_colors = FALSE;
+
+        // Cheater!!!
+        // TODO: find way how to make this better than simple
+        for(uint8_t color=0; color < 3; color++){
+            for(uint8_t led_index=0; led_index < LEDS_COUNT; led_index++){
+                colors[led_index][color] = colors_new[led_index][color];
+            }
+        }
+    }
 }
 
 
 static inline void PWM()
 {
-	if(++pwm_level >= pwm_level_max){
-		pwm_level = 0x00;
+    if(++pwm_level >= pwm_level_max){
+        pwm_level = 0x00;
 
-		if(update_colors){
-			if(smooth_delay != 0){
-				SmoothlyUpdateColors();
-			}
-		}
-	}
+        if(update_colors){
+            if(smooth_delay != 0){
+                SmoothlyUpdateColors();
+            }
+        }
+    }
 
     // TODO: Check I/O of the 74HC595, skip QH or not?
-	// Skip I/O - QH of 74HC595 (IC5, IC6)
-	HC595_CLK_DOWN;
-	HC595_DATA_PORT = 0x00;
-	HC595_CLK_UP;
+    // Skip I/O - QH of 74HC595 (IC5, IC6)
+    HC595_CLK_DOWN;
+    HC595_DATA_PORT = 0x00;
+    HC595_CLK_UP;
 
+    if(smooth_delay == 0){
+        // Set I/O - QH, QF, QE of 74HC595 (IC5, IC6)
+        for(uint8_t color=0; color<3; color++){
+            uint8_t hc595_data = 0x00;
 
-	uint8_t hc595_data = 0x00;
+            HC595_CLK_DOWN;
+            if(colors_new[LED2][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
+            if(colors_new[LED4][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
+            if(colors_new[LED6][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
+            if(colors_new[LED8][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
+            HC595_DATA_PORT = hc595_data;
+            HC595_CLK_UP;
+        }
 
-	if(smooth_delay == 0){
-		// Set I/O - QH, QF, QE of 74HC595 (IC5, IC6)
-		for(uint8_t color=0; color<3; color++){
-			hc595_data = 0x00;
+        // Set I/O - QD, QC, QB of 74HC595 (IC5, IC6)
+        for(uint8_t color=0; color<3; color++){
+            uint8_t hc595_data = 0x00;
 
-			HC595_CLK_DOWN;
-			if(colors_new[LED2][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
-			if(colors_new[LED4][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
-			if(colors_new[LED6][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
-			if(colors_new[LED8][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
-			HC595_DATA_PORT = hc595_data;
-			HC595_CLK_UP;
-		}
+            HC595_CLK_DOWN;
+            if(colors_new[LED1][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
+            if(colors_new[LED3][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
+            if(colors_new[LED5][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
+            if(colors_new[LED7][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
+            HC595_DATA_PORT = hc595_data;
+            HC595_CLK_UP;
+        }
+    }else{
+        // Set I/O - QH, QF, QE of 74HC595 (IC5, IC6)
+        for(uint8_t color=0; color<3; color++){
+            uint8_t hc595_data = 0x00;
 
-		// Set I/O - QD, QC, QB of 74HC595 (IC5, IC6)
-		for(uint8_t color=0; color<3; color++){
-			hc595_data = 0x00;
+            HC595_CLK_DOWN;
+            if(colors[LED2][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
+            if(colors[LED4][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
+            if(colors[LED6][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
+            if(colors[LED8][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
+            HC595_DATA_PORT = hc595_data;
+            HC595_CLK_UP;
+        }
 
-			HC595_CLK_DOWN;
-			if(colors_new[LED1][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
-			if(colors_new[LED3][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
-			if(colors_new[LED5][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
-			if(colors_new[LED7][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
-			HC595_DATA_PORT = hc595_data;
-			HC595_CLK_UP;
-		}
-	}else{
-		// Set I/O - QH, QF, QE of 74HC595 (IC5, IC6)
-		for(uint8_t color=0; color<3; color++){
-			uint8_t hc595_data = 0x00;
+        // Set I/O - QD, QC, QB of 74HC595 (IC5, IC6)
+        for(uint8_t color=0; color<3; color++){
+            uint8_t hc595_data = 0x00;
 
-			HC595_CLK_DOWN;
-			if(colors[LED2][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
-			if(colors[LED4][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
-			if(colors[LED6][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
-			if(colors[LED8][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
-			HC595_DATA_PORT = hc595_data;
-			HC595_CLK_UP;
-		}
+            HC595_CLK_DOWN;
+            if(colors[LED1][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
+            if(colors[LED3][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
+            if(colors[LED5][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
+            if(colors[LED7][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
+            HC595_DATA_PORT = hc595_data;
+            HC595_CLK_UP;
+        }
+    }
 
-		// Set I/O - QD, QC, QB of 74HC595 (IC5, IC6)
-		for(uint8_t color=0; color<3; color++){
-			uint8_t hc595_data = 0x00;
+    // Skip I/O - QA of 74HC595 (IC5, IC6)
+    HC595_CLK_DOWN;
+    HC595_DATA_PORT = 0x00;
+    HC595_CLK_UP;
 
-			HC595_CLK_DOWN;
-			if(colors[LED1][color] > pwm_level)  hc595_data |= HC595_DATA0_PIN;
-			if(colors[LED3][color] > pwm_level)  hc595_data |= HC595_DATA1_PIN;
-			if(colors[LED5][color] > pwm_level)  hc595_data |= HC595_DATA2_PIN;
-			if(colors[LED7][color] > pwm_level)  hc595_data |= HC595_DATA3_PIN;
-			HC595_DATA_PORT = hc595_data;
-			HC595_CLK_UP;
-		}
-	}
-
-	// Skip I/O - QA of 74HC595 (IC5, IC6)
-	HC595_CLK_DOWN;
-	HC595_DATA_PORT = 0x00;
-	HC595_CLK_UP;
-
-	HC595_LATCH_PULSE;
+    HC595_LATCH_PULSE;
 }
 
 
 
 void SetAllLedsColors(uint8_t red, uint8_t green, uint8_t blue)
 {
-	for(uint8_t i=0; i<LEDS_COUNT; i++){
-		if(smooth_delay == 0){
-			colors_new[i][R] = red;
-			colors_new[i][G] = green;
-			colors_new[i][B] = blue;
-		}else{
-			colors[i][R] = red;
-			colors[i][G] = green;
-			colors[i][B] = blue;
-		}
-	}
+    for(uint8_t i=0; i<LEDS_COUNT; i++){
+        if(smooth_delay == 0){
+            colors_new[i][R] = red;
+            colors_new[i][G] = green;
+            colors_new[i][B] = blue;
+        }else{
+            colors[i][R] = red;
+            colors[i][G] = green;
+            colors[i][B] = blue;
+        }
+    }
 }
 
 
@@ -246,43 +252,51 @@ static inline void SmoothlyShowRGB()
 
 // TODO: rewrite it on timer interrupts
 
-	uint8_t fsm = 1, red = 0, green = 0, blue = 0;
+    uint8_t fsm = 1, red = 0, green = 0, blue = 0;
 
-	SetAllLedsColors(0, 0, 0);
-	_delay_ms(10);
+    SetAllLedsColors(0, 0, 0);
+    _delay_ms(10);
 
-	for(;fsm != 0;){
-		switch(fsm){
-		case 1:
-			SetAllLedsColors(red++, green, blue);
-			if(red >= pwm_level_max){
-				fsm = 2;
-			}
-			break;
-		case 2:
-			SetAllLedsColors(red--, green++, blue);
-			if(red == 0){
-				fsm = 3;
-			}
-			break;
-		case 3:
-			SetAllLedsColors(red, green--, blue++);
-			if(green == 0){
-				fsm = 4;
-			}
-			break;
-		case 4:
-			SetAllLedsColors(red, green, blue--);
-			if(blue == 0){
-				fsm = 0;
-			}
-			break;
-		default: fsm = 0; break;
-		}
-		_delay_ms(3);
-	}
+    for(;fsm != 0;){
+        switch(fsm){
+        case 1:
+            SetAllLedsColors(red++, green, blue);
+            if(red >= pwm_level_max){
+                fsm = 2;
+            }
+            break;
+        case 2:
+            SetAllLedsColors(red--, green++, blue);
+            if(red == 0){
+                fsm = 3;
+            }
+            break;
+        case 3:
+            SetAllLedsColors(red, green--, blue++);
+            if(green == 0){
+                fsm = 4;
+            }
+            break;
+        case 4:
+            SetAllLedsColors(red, green, blue--);
+            if(blue == 0){
+                fsm = 0;
+            }
+            break;
+        default: fsm = 0; break;
+        }
+        _delay_ms(10);
+    }
 
-	SetAllLedsColors(0, 0, 0);
+    SetAllLedsColors(0, 0, 0);
+}
+
+
+void UartSendByte(uint8_t byte)
+{
+    while((UCSRA & _BV(UDRE)) == 0x00);
+
+    UDR = byte;
 }
 
 
@@ -291,45 +305,46 @@ static inline void SmoothlyShowRGB()
 //
 ISR( TIMER1_COMPA_vect )
 {
-//	TEST_UP();
+    // Set next PWM states for all channels
+    PWM();
 
-	// Set next PWM states for all channels
-	PWM();
-
-
-
-
-	// Clear timer counter
-	TCNT1 = 0x0000;
+    // Clear timer counter
+    TCNT1 = 0x0000;
 }
 
 
 
 ISR( USART_RXC_vect )
 {
-	uint8_t rx = UDR;
-    if(rx == 0xff){
-    	receive_led_index = 0x00;
-    	receive_color_index = 0x00;
-    }else{
-    	TEST_UP();
-		if(smooth_delay == 0){
-			colors_new[receive_led_index][receive_color_index] = rx;
-		}else{
-			colors[receive_led_index][receive_color_index] = rx;
-		}
+    uint8_t ucsra = UCSRA;
+    uint8_t rx = UDR;
 
-    	if(++receive_color_index == 3){
-    		receive_color_index = 0x00;
-    		if(++receive_led_index == LEDS_COUNT){
-    			receive_led_index = 0x00;
-    			update_colors = TRUE;
-    		}
-    	}
+    sei(); // for PWM
+
+    if(ucsra & _BV(DOR) || ucsra & _BV(FE) || ucsra & _BV(PE)){
+        TEST_UP();
+        UartSendByte(RECEIVE_FAIL);
+    }else{
+        UartSendByte(RECEIVE_OK); // test on flickering and move it to the end of this branch
+
+        if(rx == START_OF_FRAME_COLORS){
+            receive_led_index = 0x00;
+            receive_color_index = 0x00;
+        }else{
+            colors_new[receive_led_index][receive_color_index] = rx;
+
+            if(++receive_color_index == 3){
+                receive_color_index = 0x00;
+                if(++receive_led_index == LEDS_COUNT){
+                    receive_led_index = 0x00;
+                    update_colors = TRUE;
+
+                    UpdateSmoothStep();
+                }
+            }
+        }
     }
     TEST_DOWN();
-
-	//UpdateSmoothStep();
 }
 
 
@@ -338,50 +353,53 @@ ISR( USART_RXC_vect )
 
 static inline void TimerForPWM_Init()
 {
-	TCCR1A = 0x00;
+    TCCR1A = 0x00;
 
-	// Default values of timer prescaller and output compare register
-	TCCR1B = _BV(CS11); // 8
-	OCR1A = 100;
+    // Default values of timer prescaller and output compare register
+    TCCR1B = _BV(CS11); // 8
+    OCR1A = 200;
 
-	TCNT1 = 0x0000;
-	TIMSK = _BV(OCIE1A);
+    TCNT1 = 0x0000;
+    TIMSK = _BV(OCIE1A);
 }
 
 static inline void HC595_Init(void)
 {
-	HC595_LATCH_DOWN;
-	HC595_CLK_DOWN;
-	HC595_OUT_ENABLE;
-	HC595_DDR |= HC595_CLK_PIN | HC595_LATCH_PIN | HC595_OUT_EN_PIN;
-	HC595_DATA_PORT = 0x00;
-	HC595_DATA_DDR = HC595_DATA0_PIN | HC595_DATA1_PIN | HC595_DATA2_PIN | HC595_DATA3_PIN;
+    HC595_LATCH_DOWN;
+    HC595_CLK_DOWN;
+    HC595_OUT_ENABLE;
+    HC595_DDR |= HC595_CLK_PIN | HC595_LATCH_PIN | HC595_OUT_EN_PIN;
+    HC595_DATA_PORT = 0x00;
+    HC595_DATA_DDR = HC595_DATA0_PIN | HC595_DATA1_PIN | HC595_DATA2_PIN | HC595_DATA3_PIN;
 }
 
-static inline void UartInitRX()
+static inline void UartInit()
 {
 #define BAUD 115200
 #include <util/setbaud.h>
-	UBRRL = UBRRL_VALUE;
-	UBRRH = UBRRH_VALUE;
+    UBRRL = UBRRL_VALUE;
+    UBRRH = UBRRH_VALUE;
 #if USE_2X
-	UCSRA |= (1 << U2X);
+    UCSRA |= (1 << U2X);
 #else
-	UCSRA &= ~(1 << U2X);
+    UCSRA &= ~(1 << U2X);
 #endif
 
-	UCSRB = _BV(RXEN) | _BV(RXCIE);
-	UCSRC = _BV(URSEL) | _BV(USBS) | _BV(UCSZ1) | _BV(UCSZ0);
+    // TX, RX Enabled, RX interrupts
+    UCSRB = _BV(TXEN) | _BV(RXEN) | _BV(RXCIE);
+
+    // Even Parity, 2-stop bits, 8 bit
+    UCSRC = _BV(URSEL) | _BV(UPM1) | _BV(USBS) | _BV(UCSZ1) | _BV(UCSZ0);
 }
 
 //
-//	Initializing I/O, USB and starts PWM generation
+//    Initializing I/O, USB and starts PWM generation
 //
 int main(void)
 {
-	// Input/Output Ports initialization
-	PORTB = 0x00;
-	DDRB = 0x00;
+    // Input/Output Ports initialization
+    PORTB = 0x00;
+    DDRB = 0x00;
 
     PORTC = 0x00;
     DDRC = 0x00;
@@ -396,26 +414,23 @@ int main(void)
     // HC595 ports initialization
     HC595_Init();
 
-    // Initialize timer used to for generate the PWM
+    // Initialize timer for PWM
     TimerForPWM_Init();
     
-    UartInitRX();
+    UartInit();
 
     // Enable interrupts
-	sei(); 
-	
-//SmoothlyShowRGB();
+    sei();
 
-	smooth_delay = 0x00;
+    smooth_delay = 0xff;
 
-	SetAllLedsColors(10, 20, 10);
+    SmoothlyShowRGB();
 
-   	for(;;){
-   	    // Wait interrupts for PWM generation
-   	    
-   	    // TODO: Syncronize the UART transfer and PWM
-	}
 
-	return 0;
+    for(;;){
+           // Wait interrupts for PWM and UART
+    }
+
+    return 0;
 }
 
