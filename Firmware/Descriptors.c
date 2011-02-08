204a51fe9009b5f1b22d2c66f9b818b9b1bcbb21
Mike Shatohin <brunql@gmail.com>
2011-02-08 22:50:40 +0300

    Firmware: Descriptors.c: set max power consumption to 500mA

--- Firmware/Descriptors.c
+++ Firmware/Descriptors.c
@@ -115,7 +115,7 @@ USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
 
 			.ConfigAttributes       = (USB_CONFIG_ATTR_BUSPOWERED | USB_CONFIG_ATTR_SELFPOWERED),
 
-			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(100)
+			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(500)
 		},
 
 	.HID_Interface =
