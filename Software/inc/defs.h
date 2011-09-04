37a8d82efb1146d4db2c57671ea4c7484aa0bf29
Mike Shatohin <brunql@gmail.com>
2011-09-04 12:36:57 +0400

    Software: defs.h: add #pragma once and copyright notice;

--- Software/inc/defs.h
+++ Software/inc/defs.h
@@ -1,6 +1,32 @@
-#ifndef DEFS_H
-#define DEFS_H
-#include<qglobal.h>
+/*
+ * defs.h
+ *
+ *  Created on: 17.04.2011
+ *      Author: Timur Sattarov && Mike Shatohin
+ *     Project: Lightpack
+ *
+ *  Lightpack is very simple implementation of the backlight for a laptop
+ *
+ *  Copyright (c) 2011 Mike Shatohin, mikeshatohin [at] gmail.com
+ *
+ *  Lightpack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lightpack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <qglobal.h>
 
 #define QT_GRAB_SUPPORT
 #ifdef Q_WS_WIN
@@ -18,5 +44,3 @@
 #ifdef Q_WS_WIN
 #   define ALIEN_FX_SUPPORTED
 #endif
-
-#endif // DEFS_H
