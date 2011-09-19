dc807d1aa7bdd1ddfc6508b341072b626ed979d0
Timur Sattarov <tim.helloworld@gmail.com>
2011-09-19 19:19:42 +0400

    Software: license has been added to X11Grabber.hpp

--- Software/src/grab/X11Grabber.cpp
+++ Software/src/grab/X11Grabber.cpp
@@ -1,3 +1,28 @@
+/*
+ * X11Grabber.hpp
+ *
+ *  Created on:
+ *     Project: Lightpack
+ *
+ *  Copyright (c) 2011 Andrey Isupov, Mike Shatohin, Timur Sattarov
+ *
+ *  Lightpack a USB content-driving ambient lighting system
+ *
+ *  Lightpack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lightpack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include"X11Grabber.hpp"
 
 #ifdef X11_GRAB_SUPPORT
