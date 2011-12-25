5cf80b511e4c7394fe72ab92f73d926bcbc5135f
Mike Shatohin <brunql@gmail.com>
2011-12-25 13:40:11 +0300

    Software: build-deb.sh: add auto-renaming "Lightpack" to "lightpack" and check exists "lightpack" in "deb/usr/bin";

--- Software/build-deb.sh
+++ Software/build-deb.sh
@@ -1,10 +1,29 @@
+#
+# lightpack/Software/build-deb.sh
+#
+#     Author: brunql
+# Created on: 25.12.11
+#
+
+if [ -e "deb/usr/bin/Lightpack" ];
+then
+	echo "Renaming 'deb/usr/bin/Lightpack' to 'deb/usr/bin/lightpack'."
+	mv deb/usr/bin/Lightpack deb/usr/bin/lightpack
+fi
+
+if [ ! -e "deb/usr/bin/lightpack" ];
+then
+	echo "File 'deb/usr/bin/lightpack' not found."
+	exit 1
+fi
+
 if [ -x "`which md5deep 2>/dev/null`" ]; 
 then
 	# Update MD5 sums
 	md5deep -r deb/usr > deb/DEBIAN/md5sums
 else
 	echo "Please install 'md5deep' package first."
-	exit 1
+	exit 2
 fi
 
 # Build package in format "name_version_arch.deb"
