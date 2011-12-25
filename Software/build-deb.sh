a689029cc7d88e28e475fd8e2660ce54e1e16b79
Mike Shatohin <brunql@gmail.com>
2011-12-25 12:38:24 +0300

    Software: add check the availability of md5deep tool in build-deb.sh;

--- Software/build-deb.sh
+++ Software/build-deb.sh
@@ -1,5 +1,11 @@
-# Update MD5 sums
-md5deep -r deb/usr > deb/DEBIAN/md5sums
+if [ -x "`which md5deep 2>/dev/null`" ]; 
+then
+	# Update MD5 sums
+	md5deep -r deb/usr > deb/DEBIAN/md5sums
+else
+	echo "Please install 'md5deep' package first."
+	exit 1
+fi
 
 # Build package in format "name_version_arch.deb"
 fakeroot dpkg-deb --build deb/ .
