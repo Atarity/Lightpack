875362f10883d6e74e86652057b851f0bbf26fb7
Timur Sattarov <tim.helloworld@gmail.com>
2012-11-07 15:46:02 +0400

    minor test fix

--- Software/tests/GrabCalculationTest.cpp
+++ Software/tests/GrabCalculationTest.cpp
@@ -7,6 +7,4 @@ void GrabCalculationTest::testCase1()
     memset(buf, 0xfa, 16);
     QVERIFY2(Grab::Calculations::calculateAvgColor(&result, buf, Grab::BufferFormatArgb, 16, QRect(0,0,4,1)) == 0, "Failure. calculateAvgColor returned wrong errorcode");
     QCOMPARE(result, qRgb(0xfa,0xfa,0xfa));
-    QVERIFY2(Grab::Calculations::calculateAvgColor(&result, buf, Grab::BufferFormatAbgr, 16, QRect(0,0,4,1)) == 0, "Failure. calculateAvgColor returned wrong errorcode");
-    QCOMPARE(result, qRgb(0xfa,0xfa,0xfa));
 }
