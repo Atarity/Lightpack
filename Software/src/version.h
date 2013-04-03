168b8c97e2d3326c46fc591e024b745fd0357da9
Timur Sattarov <tim.helloworld@gmail.com>
2013-04-03 20:31:22 +0400

    version updated to 5.9.5

--- Software/src/version.h
+++ Software/src/version.h
@@ -27,10 +27,10 @@
 #pragma once
 
 // Current version of Lightpack software
-#define VERSION_STR     "5.9.4"
+#define VERSION_STR     "5.9.5"
 
 // Resource defines using in Lightpack.rc
-#define VER_FILEVERSION             5,9,4,0
+#define VER_FILEVERSION             5,9,5,0
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 
 #define VER_FILEVERSION_STR         VERSION_STR "\0"
