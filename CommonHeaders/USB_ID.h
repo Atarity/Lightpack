39d82515111e46e0f878eeb485821fbdaf8a839b
Timur Sattarov <tim.helloworld@gmail.com>
2013-08-09 19:21:11 +0400

    Openmoko's VID:PID has been added

--- CommonHeaders/USB_ID.h
+++ CommonHeaders/USB_ID.h
@@ -5,9 +5,7 @@
  *      Author: Mike Shatohin (brunql)
  *     Project: Lightpack
  *
- *  Lightpack? I wanna own USB Vendor ID! }:>
- *
- *  Copyright (c) 2011 Mike Shatohin, mikeshatohin [at] gmail.com
+ *  Copyright (c) 2011 Mike Shatohin, mikeshatohin [at] gmail.com, Timur Sattarov (tim.helloworld at gmail.com)
  *
  *  Lightpack is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -28,11 +26,16 @@
 #ifndef USB_ID_H_
 #define USB_ID_H_
 
-/* Lightpack using Atmel VID:PID pair */
+/* earlier version of Lightpack used to use Atmel's VID:PID pair */
 /* find it in GenericHID demo from LUFA project */
 
-#define USB_VENDOR_ID   0x03EB
-#define USB_PRODUCT_ID  0x204F
+#define USB_OLD_VENDOR_ID   0x03EB
+#define USB_OLD_PRODUCT_ID  0x204F
+
+/* Openmoko's free VID:PID http://wiki.openmoko.org/wiki/USB_Product_IDs */
+
+#define USB_VENDOR_ID   0x1D50
+#define USB_PRODUCT_ID  0x6022
 
 #define USB_MANUFACTURER_STRING     "lightpack.googlecode.com"
 #define USB_PRODUCT_STRING          "Lightpack"
