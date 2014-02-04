f87bc61e2e91f419d57a9d4f7bb1f2ed0d78234c
Timur Sattarov <tim.helloworld@gmail.com>
2014-02-05 01:39:16 +0400

    temporarly removed xxxEachWidget grabbers

--- Software/grab/WinUtils.cpp
+++ Software/grab/WinUtils.cpp
@@ -37,7 +37,7 @@ namespace WinUtils
 {
 
 const WCHAR lightpackHooksDllName[] = L"prismatik-hooks.dll";
-static LPCWSTR pwstrExcludeProcesses[]={L"skype.exe", L"chrome.exe", L"firefox.exe", L"iexplore.exe", L""};
+static LPCWSTR pwstrExcludeProcesses[]={L"skype.exe", L"chrome.exe", L"firefox.exe", L"iexplore.exe", L"qtcreator.exe"};
 
 BOOL SetPrivilege(HANDLE hToken, LPCTSTR szPrivName, BOOL fEnable) {
 
