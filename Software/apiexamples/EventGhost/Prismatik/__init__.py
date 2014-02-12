ba33d03004bc6dc69e1909a8d4a8e390123b546f
Elad Shahar <SaladFork@Gmail.com>
2014-02-12 16:40:04 -0500

    Add new author name

--- Software/apiexamples/EventGhost/Prismatik/__init__.py
+++ Software/apiexamples/EventGhost/Prismatik/__init__.py
@@ -3,7 +3,7 @@ import lightpack
 
 eg.RegisterPlugin(
 	name = "Prismatik for Eventghost",
-	author = "Lucleonhart, 0xdefec",
+	author = "Lucleonhart, 0xdefec, SaladFork",
 	version = "1.0",
 	kind = "external",
 	description = "Control the Lightpack's Prismatik software. Enable API Server in Prismatik's experimental settings (leave the key empty and port at 3636)."
@@ -18,7 +18,7 @@ class Lightpack(eg.PluginBase):
 		self.AddAction(LowBrightness)
 		self.AddAction(MaxBrightness)
 		self.AddAction(NextProfile)
-		
+
 		self.lpack = lightpack.lightpack("127.0.0.1", 3636, "", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] )
 		self.lpack.connect()
 
@@ -35,10 +35,10 @@ class TurnOn(eg.ActionBase):
 		print "Lightpack lights are on now"
 
 class TurnOff(eg.ActionBase):
-	
+
 	name = "Turn off"
 	description = "Turn off the Lights."
-	
+
 	def __call__(self):
 		self.plugin.lpack.lock()
 		self.plugin.lpack.turnOff()
@@ -46,10 +46,10 @@ class TurnOff(eg.ActionBase):
 		print "Lightpack lights are off now"
 
 class Toggle(eg.ActionBase):
-	
+
 	name = "Toggle lights on/off"
 	description = "Toggles the lights on or off"
-	
+
 	def __call__(self):
 		self.plugin.lpack.lock()
 		status = self.plugin.lpack.getStatus()
@@ -63,10 +63,10 @@ class Toggle(eg.ActionBase):
 		print "Lightpack lights are "+self.plugin.lpack.getStatus().strip()+" now"
 
 class LowBrightness(eg.ActionBase):
-	
+
 	name = "Low Brightness"
 	description = "Sets the brightness to 20%"
-	
+
 	def __call__(self):
 		self.plugin.lpack.lock()
 		self.plugin.lpack.setBrightness(20)
@@ -74,10 +74,10 @@ class LowBrightness(eg.ActionBase):
 		print "Brightness now at 20%"
 
 class MaxBrightness(eg.ActionBase):
-	
+
 	name = "Maximum Brightness"
 	description = "Sets the brightness to 100%"
-	
+
 	def __call__(self):
 		self.plugin.lpack.lock()
 		self.plugin.lpack.setBrightness(100)
@@ -85,10 +85,10 @@ class MaxBrightness(eg.ActionBase):
 		print "Brightness now at 100%"
 
 class NextProfile(eg.ActionBase):
-	
+
 	name = "Next profile"
 	description = "Switches to the next prismatik profile"
-	
+
 	def __call__(self):
 		self.plugin.lpack.lock()
 		profiles = self.plugin.lpack.getProfiles()
