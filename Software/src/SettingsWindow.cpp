e78c89196f58cae4c9b337f9e1ee6aa64b630519
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2014-06-06 17:55:38 +0400

    Fix incorrect version displaying in Settings->About
    Sometimes Prismatik couldn't read version from Lightpack HID device.
    In this case it reopens the device and eventually reads the version
    information. Unfortunately, there was an issue that prevented
    updating version string in UI more than once, due to "spoiled" template
    string.

--- Software/src/SettingsWindow.cpp
+++ Software/src/SettingsWindow.cpp
@@ -1693,8 +1693,8 @@ void SettingsWindow::versionsUpdate()
 {
     DEBUG_LOW_LEVEL << Q_FUNC_INFO;
 
-    // Save templete for construct version string
-    QString versionsTemplate = ui->labelVersions->text();
+    // use template to construct version string
+    QString versionsTemplate = tr("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\"> <html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\"> p, li { white-space: pre-wrap; } </style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\"> <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">software <span style=\" font-size:8pt; font-weight:600;\">%1</span><span style=\" font-size:8pt;\"> (revision </span><a href=\"http://code.google.com/p/lightpack/source/detail?r=%2\"><span style=\" font-size:8pt; text-decoration: underline; color:#0000ff;\">%2 </span></a><span style=\" font-size:8pt;\">), firmware <b>%3</b></span></p></body></html>");
 
 #ifdef GIT_REVISION
     versionsTemplate = versionsTemplate.arg(
